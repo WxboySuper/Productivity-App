72d3d62be14cc87f84d880323fb6031d89b1e35b
WeatherboySuper <SuperPlays1020@gmail.com>
2024-11-10 21:16:51 -0600

    Fix Tests

--- python/tests/test_database.py
+++ python/tests/test_database.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import unittest
 import sqlite3
 from datetime import datetime
--- python/tests/test_todo.py
+++ python/tests/test_todo.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import unittest
 from datetime import datetime
 from unittest.mock import Mock, patch
