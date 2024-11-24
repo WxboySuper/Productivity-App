7a38e4d22133f692d1adb706fda31ab1e6d07ddd
WeatherboySuper <SuperPlays1020@gmail.com>
2024-11-24 10:29:44 -0600

    Fix and add logging to preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,10 +1,33 @@
 const { contextBridge, ipcRenderer } = require('electron')
+const log = require('electron-log')
 
-contextBridge.exposeInMainWorld('api', {
-    // Add any API methods you want to expose to the renderer process
-    // For example:
-    getTodos: () => ipcRenderer.invoke('get-todos'),
-    addTodo: (todo) => ipcRenderer.invoke('add-todo', todo),
-    updateTodo: (todo) => ipcRenderer.invoke('update-todo', todo),
-    deleteTodo: (id) => ipcRenderer.invoke('delete-todo', id)
-})
+log.transports.file.level = 'info'
+log.info('preload.js - Preload script initializing')
+
+// Define the API methods
+const api = {
+    getTodos: async () => {
+        log.info('preload.js - getTodos called from renderer')
+        // skipcq: JS-0111
+        return await ipcRenderer.invoke('get-todos')
+    },
+    addTodo: async (todo) => {
+        log.info('preload.js - addTodo called with:', todo)
+        // skipcq: JS-0111
+        return await ipcRenderer.invoke('add-todo', todo)
+    },
+    updateTodo: async (todo) => {
+        log.info('preload.js - updateTodo called with:', todo)
+        // skipcq: JS-0111
+        return await ipcRenderer.invoke('update-todo', todo)
+    },
+    deleteTodo: async (id) => {
+        log.info('preload.js - deleteTodo called with id:', id)
+        // skipcq: JS-0111
+        return await ipcRenderer.invoke('delete-todo', id)
+    }
+}
+
+// Expose the API to the renderer process
+contextBridge.exposeInMainWorld('api', api)
+log.info('API exposed to renderer process')
