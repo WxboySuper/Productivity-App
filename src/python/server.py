be629a07f3e3218c03aafa4ee51d9998a5812b8c
WeatherboySuper <SuperPlays1020@gmail.com>
2024-12-23 08:57:32 -0600

    Simplify server execution by removing the run_server function and directly calling app.run()

--- src/python/server.py
+++ src/python/server.py
@@ -3,11 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 app.config['PORT'] = 5000
 
-
-def run_server():
-    """Run the Flask server."""
-    app.run(port=app.config['PORT'])
-
-
-if __name__ == '__main__':
-    run_server()
+if __name__ == '__main__': # pragma: no cover
+    app.run(port=app.config['PORT']) # pragma: no cover
